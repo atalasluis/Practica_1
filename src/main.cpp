ddc0f9feb79b4c9a897f0e9c629d3f306b1a8c84
atalasluis <atalasturey@gmail.com>
2026-03-08 10:52:41 -0400

    clase ControlLeds y SensorUltrasonico

---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+#include <Arduino.h>
+
+// ===== Clase SensorUltrasonico =====
+class SensorUltrasonico {
+  private:
+    int pinTrig;
+    int pinEcho;
+
+  public:
+    SensorUltrasonico(int trig, int echo) {
+      pinTrig = trig;
+      pinEcho = echo;
+    }
+
+    void begin() {
+      pinMode(pinTrig, OUTPUT);
+      pinMode(pinEcho, INPUT);
+    }
+
+    float medirDistancia() {
+      digitalWrite(pinTrig, LOW);
+      delayMicroseconds(2);
+      digitalWrite(pinTrig, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(pinTrig, LOW);
+
+      long duracion = pulseIn(pinEcho, HIGH);
+      float distancia = duracion * 0.034 / 2; // cm
+
+      return distancia;
+    }
+};
+
+// ===== Clase ControlLeds =====
+class ControlLeds {
+  private:
+    int led1, led2, led3;
+
+  public:
+    ControlLeds(int l1, int l2, int l3) {
+      led1 = l1;
+      led2 = l2;
+      led3 = l3;
+    }
+
+    void begin() {
+      pinMode(led1, OUTPUT);
+      pinMode(led2, OUTPUT);
+      pinMode(led3, OUTPUT);
+    }
+
+    void apagarTodos() {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+    }
+
+    void encenderNivel(int nivel) {
+      apagarTodos();
+
+      if (nivel >= 1) digitalWrite(led1, HIGH);
+      if (nivel >= 2) digitalWrite(led2, HIGH);
+      if (nivel >= 3) digitalWrite(led3, HIGH);
+    }
+
+    void parpadearTodos() {
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      delay(200);
+
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      delay(200);
+    }
+};
+
+// ===== Objetos =====
+SensorUltrasonico sensor(3, 2);
+ControlLeds leds(4, 5, 6);
+
+// ===== Setup =====
+void setup() {
+  Serial.begin(9600);
+  sensor.begin();
+  leds.begin();
+}
+
+// ===== Loop =====
+void loop() {
+  float distancia = sensor.medirDistancia();
+  Serial.println(distancia);
+
+  if (distancia > 30) {
+    leds.apagarTodos();
+  }
+  else if (distancia > 20) {
+    leds.encenderNivel(1);
+  }
+  else if (distancia > 10) {
+    leds.encenderNivel(2);
+  }
+  else if (distancia > 5) {
+    leds.encenderNivel(3);
+  }
+  else {
+    leds.parpadearTodos();
+  }
+
+  delay(100);
+}
